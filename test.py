164dde896a33308488678d86153be68288589a75
Changxing Cao <cc5766@nyu.edu>
2019-03-07 16:46:14 -0500

    use soup find instead of expression

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import urllib
 import urllib2
 import cookielib
 import re
+from bs4 import BeautifulSoup
 
 cookie = cookielib.CookieJar()
 handler=urllib2.HTTPCookieProcessor(cookie)
@@ -33,10 +34,45 @@ data = urllib.urlencode(values)
 opener.open(url, data)
 # url_correction="https://www.medanswering.com/admincorrectionrequests.taf?_function=transproviderlist&"
 # result = opener.open(url_correction)
-url = 'https://www.medanswering.com/admincorrectionrequests.taf?_function=transproviderlist&_start=1201'
+url = 'https://www.medanswering.com/admincorrectionrequests.taf?_function=transproviderlist&_start=001'
 result = opener.open(url)
 content = result.read()
-pattern = re.compile('<tr.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?<a.*?>(.*?)</a>.*?</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?</tr>', re.S)
+
+soup = BeautifulSoup(content, 'lxml')
+table_corr = soup.find('table', attrs={'border': "0", 'cellspacing': "1", 'cellpadding': "3"})
+# corrs = table_corr.findChildren('tr')
+print(table_corr)
+pattern = re.compile('<tr.*?>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?<td.*?<a.*?>(.*?)</a>.*?</td>.*?<td.*?>(.*?)</td>.*?<td.*?>(.*?)</td>.*?</tr>', re.S)
+
+
+d1 = []
+d2 = []
+d3 = []
+d4 = []
+d5 = []
+d6 = []
+d7 = []
+d8 = []
+d9 = []
 items = re.findall(pattern, content)
+print len(items)
+i = 0
 for item in items:
-    print(item)
+
+    if i == 0:
+        # for i in range(9):
+        #     print i, '+++++++++', item[i]
+        i = -1
+        continue
+
+    d1.append(item[0].strip())
+    d2.append(item[1].strip())
+    d3.append(item[2].strip())
+    d4.append(item[3].strip())
+    d5.append(item[4].strip())
+    d6.append(item[5].strip())
+    d7.append(item[6].strip())
+    d8.append(item[7].strip())
+    d9.append(item[8].strip())
+
+# print(d7)
